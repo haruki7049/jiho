ad1a818064ad4206b045b8e505ff75ee0cce74f3
haruki7049 <tontonkirikiri@gmail.com>
2025-07-30 19:42:28 +0900

    UpdatE

--- src/main/java/dev/haruki7049/jiho/App.java
+++ src/main/java/dev/haruki7049/jiho/App.java
@@ -1,7 +1,31 @@
 package dev.haruki7049.jiho;
 
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("HOGE");
+    CommandLine cli = new CommandLine(new CLI());
+    System.exit(cli.execute(args));
+  }
+}
+
+@CommandLine.Command(name = "jiho", version = "0.1.0")
+class CLI implements Callable<Integer> {
+  @CommandLine.Option(
+      names = {"-h", "--help"},
+      description = "show this help",
+      usageHelp = true)
+  boolean showHelp;
+
+  @CommandLine.Option(
+      names = {"-v", "--version"},
+      description = "show this command's version",
+      versionHelp = true)
+  boolean showVersion;
+
+  @Override
+  public Integer call() throws Exception {
+    return 0;
   }
 }
