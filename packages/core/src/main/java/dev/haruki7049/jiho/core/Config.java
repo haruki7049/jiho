b78ffd9d45faf85f6a40d5309de628196bd0821f
haruki7049 <tontonkirikiri@gmail.com>
2025-11-04 23:04:45 +0900

    refactor(Config): Refactor the constructor

--- packages/core/src/main/java/dev/haruki7049/jiho/core/Config.java
+++ packages/core/src/main/java/dev/haruki7049/jiho/core/Config.java
@@ -1,5 +1,7 @@
 package dev.haruki7049.jiho.core;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.dirs.ProjectDirectories;
 import java.io.File;
 import java.nio.file.Path;
@@ -10,7 +12,23 @@ public class Config {
   private static final ProjectDirectories projDirs =
       ProjectDirectories.from("dev", "haruki7049", "jiho");
 
-  public Path dataDir = Paths.get(projDirs.dataDir);
-  public File soundSource;
-  public Duration duration = Duration.ofSeconds(1);
+  public final Path dataDir;
+  public final File soundSource;
+  public final Duration duration;
+
+  @JsonCreator
+  public Config(
+      @JsonProperty("dataDir") Path dataDir,
+      @JsonProperty("soundSource") File soundSource,
+      @JsonProperty("duration") Duration duration)
+      throws RuntimeException {
+
+    this.dataDir = this.getOrDefault(dataDir, Paths.get(projDirs.dataDir));
+    this.soundSource = soundSource;
+    this.duration = this.getOrDefault(duration, Duration.ofSeconds(1));
+  }
+
+  private <T> T getOrDefault(T value, T defaultValue) {
+    return (value != null) ? value : defaultValue;
+  }
 }
