0fa79b8bab7ac76b78b09b2d927cd39091d252ee
haruki7049 <tontonkirikiri@gmail.com>
2025-11-06 20:04:31 +0900

    fix(AudioSourceIsNullException.java): Add javadoc comment for checkstyle lint

--- packages/core/src/main/java/dev/haruki7049/jiho/core/AudioSourceIsNullException.java
+++ packages/core/src/main/java/dev/haruki7049/jiho/core/AudioSourceIsNullException.java
@@ -1,6 +1,9 @@
 package dev.haruki7049.jiho.core;
 
+/** Exception thrown when the audio source file is null or does not exist. */
 public class AudioSourceIsNullException extends Exception {
+
+  /** Constructs the exception with a default message. */
   public AudioSourceIsNullException() {
     super(
         "Audio source is null, or there is no audio file. Please edit your configuration. In Linux,"
